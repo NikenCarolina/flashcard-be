2d722cb4b3ec1e6ab6f7b249def6729bfff0d528
Niken <63431850+NikenCarolina@users.noreply.github.com>
2024-10-18 22:59:00 +0700

    Revert "feat: init project with clean architecture"

--- internal/handler/user_flashcard_handler.go
+++
@@ -1,7 +0,0 @@
-package handler
-
-import "github.com/gin-gonic/gin"
-
-func (h *UserHandler) ListSets(ctx *gin.Context) {
-	panic("not implemented")
-}
--- internal/repository/card_set_repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-type CardSetRepository interface{}
-type cardSetRepository struct{}
-
-func NewCardSetRepository() *cardSetRepository {
-	return &cardSetRepository{}
-}
